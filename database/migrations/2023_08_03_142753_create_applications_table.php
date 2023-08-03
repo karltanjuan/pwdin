1e86af74fe156d4a209757785468f74c86b42198
Gail Larupay <gail.larupay@Gails-MacBook-Air.local>
2023-08-03 23:00:43 +0800

    Added application migration

---
+++ database/migrations/2023_08_03_142753_create_applications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('job_id')->unsigned();
+            $table->bigInteger('applicant_id')->unsigned();
+            $table->text('cover_letter')->nullable(true);
+            $table->string('status')->nullable(false);
+            $table->timestamps();
+
+            $table->foreign('job_id')->references('id')->on('jobs')->onDelete('cascade');
+            $table->foreign('applicant_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};
