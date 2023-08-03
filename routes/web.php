d100a367d1bb1c0527ae9721dc7a4643d65c2330
Gail Larupay <gail.larupay@Gails-MacBook-Air.local>
2023-08-03 15:54:46 +0800

    Sample push

--- routes/web.php
+++ routes/web.php
@@ -76,6 +76,7 @@ Route::group(['prefix' => 'employer'], function() {
 // Admin/Moderator
 Route::group(['prefix' => 'admin'], function() {
 
+
     Route::get('register', [AdminAuthController::class, 'getRegister'])->name('admin.getRegister');
     Route::post('postRegister', [AdminAuthController::class, 'postRegister'])->name('admin.postRegister');
     
