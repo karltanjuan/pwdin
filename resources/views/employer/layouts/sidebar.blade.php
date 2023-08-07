952369d8b46f7efc74c8a426c0682d04b2b45dd4
Karl Tanjuan <tanjuankarlangelo2015@yahoo.com>
2023-08-07 21:03:38 +0800

    Customizing sidebar dropdown

--- resources/views/employer/layouts/sidebar.blade.php
+++ resources/views/employer/layouts/sidebar.blade.php
@@ -9,27 +9,38 @@
         <ul class="navLinks">
             <li class="{{ 'employer/dashboard' == request()->path() ? 'active' : '' }} navList">
                 <a href="{{url('/employer/dashboard')}}">
-                    <ion-icon name="home-outline"></ion-icon>
+                    <i class="fa-solid fa-gauge fa-icon"></i>
                     <span class="links">Dashboard</span>
                 </a>
             </li>
             <li class="{{ 'employer/jobs' == request()->path() ? 'active' : '' }} navList">
                 <a href="{{url('/employer/jobs')}}">
-                    <ion-icon name="folder-outline"></ion-icon>
+                    <i class="fa-solid fa-briefcase fa-icon"></i>
                     <span class="links">Job Post</span>
                 </a>
             </li>
-            <li class="navList">
+            <li class="navList settings-dropdown">
                 <a href="javascript:void(0)">
-                    <ion-icon name="settings-outline"></ion-icon>
+                    <i class="fa-solid fa-gear fa-icon"></i>
                     <span class="links">Settings</span>
+                    <i class="fa-solid fa-caret-right fa-icon"></i>
+                    {{-- <i class="fa-solid fa-caret-down fa-icon"></i> --}}
                 </a>
             </li>
+            
+            <ul class="settings-dropdown-list">
+                <li class="{{ 'employer/app-status' == request()->path() ? 'active' : '' }} navList">
+                    <a href="{{url('/employer/app-status')}}">
+                        <i class="fa-solid fa-clipboard-check fa-icon"></i>
+                        <span class="links">App Status</span>
+                    </a>
+                </li>
+            </ul>
         </ul>
         <ul class="bottom-link">
             <li>
                 <a href="{{url('/employer/logout')}}">
-                    <ion-icon name="log-out-outline"></ion-icon>
+                    <i class="fa-solid fa-right-from-bracket fa-icon"></i>
                     <span class="links">Logout</span>
                 </a>
             </li>
