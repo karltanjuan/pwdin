90db981df01fa5d68faee71d1d3c941bcae7399f
Karl Angelo Tanjuan <tanjuankarlangelo2015@gmail.com>
2023-10-26 19:51:35 +0800

    Added condition on navbar button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,9 +84,13 @@
                     </div> --}}
                     <a href="{{ url('/contact') }}" class="nav-item nav-link">Contact</a>
                 </div>
-                <a href="{{ url('/choose-account') }}"
-                    class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Login Account<i
-                        class="fa fa-arrow-right ms-3"></i></a>
+                @if(auth()->check())
+                    <a href="{{ url('/applicant/jobs') }}" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Dashboard<i class="fa fa-arrow-right ms-3"></i></a>
+                @elseif(auth()->guard('employers')->check())
+                    <a href="{{ url('/employer/jobs') }}" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Dashboard<i class="fa fa-arrow-right ms-3"></i></a>
+                @else
+                    <a href="{{ url('/choose-account') }}" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Login Account<i class="fa fa-arrow-right ms-3"></i></a>
+                @endif
             </div>
         </nav>
         <!-- Navbar End -->
