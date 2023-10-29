c76cba6460a2318b8f3a1948c2ba1d55be70be5c
Karl Angelo Tanjuan <tanjuankarlangelo2015@gmail.com>
2023-10-29 14:42:20 +0800

    Refactor applicant change resume

--- resources/views/applicant/resume.blade.php
+++ resources/views/applicant/resume.blade.php
@@ -1,54 +1,51 @@
 @extends('applicant.layouts.master')
 
-@section('title', 'Applicant Change Resume')
+@section('title', 'Applicant - Change Resume')
+@section('cover_page')
+    <li class="breadcrumb-item text-white active">Settings</li>
+    <li class="breadcrumb-item text-white active">Change Resume</li>
+@endsection
 
 @section('content')
     <style>
         
     </style>
-        <div class="content-0">
-            <div class="head-container">
-                <h1>Change Resume</h1>
-            </div>
-            <div id="form">
-                <div class="form second" id="form-second">
-                    <div class="details ID">
-                        <div class="fields">
-                            <div class="input-field">
-                                <label>Upload CV</label>
-                                <input class="resume" id="resume" type="file" accept=".pdf">
-                                <span class="err-resume err-msg"></span>
-                            </div>
-                            @php
-                                $resume = str_replace('public', 'storage', auth()->user()->resume);
-                            @endphp
-                            <a href="{{asset($resume)}}" target="_blank">View Resume</a>
-                            <br><br>
-                            <button class="primary-btn btn-update">Save Resume</button>
-                        </div>
+
+    <h1 class="text-center mb-1 wow fadeInUp title-label" data-wow-delay="0.1s">Change Resume</h1>
+    <p class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s">Unlock your dream job with an outstanding resume!</p>
+
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="card">
+                <div class="card-body">
+                    {{-- Resume --}}
+                    <div class="input-group">
+                        <input type="file" class="form-control form-control-lg resume" id="resume" tabindex="18"
+                            accept=".pdf">
+                        <label class="input-group-text" for="resume">Upload CV</label>
+                    </div>
+                    <span class="err-resume err-msg mb-4"></span>
+
+                    @php
+                        $resume = str_replace('public', 'storage', auth()->user()->resume);
+                    @endphp
+                    
+                    <div class="text-center text-lg-start mt-4 pt-2">
+                        @if(!empty($resume))
+                            <a href="{{asset($resume)}}" target="_blank" type="button" class="btn btn-outline-secondary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem;">View resume</a>
+                        @endif
+                        <button type="button" class="btn-update btn btn-primary btn-lg"
+                            style="padding-left: 2.5rem; padding-right: 2.5rem;">Save Password</button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    
+    @include('applicant.layouts.scripts')
 
-        <script>
-            var err_counter = 0;
-            function displayErrors(errors) {
-                $('.err-msg').text('');
-                $('.error').css('border', 'none')
-                $('.err-msg').siblings('input').removeClass('error');
-
-                // loop all the error messages from backend to display on ui
-                $.each(errors, function(field, messages) {
-                    var errMsgSelector = '.err-' + field;
-                    var inputSelector = '#' + field;
-                    $(errMsgSelector).text(messages[0]);
-                    $(inputSelector).addClass('error');
-                });
-            }
-
-        
-            $('.btn-update').on('click', function() {
+    <script>
+         $('.btn-update').on('click', function() {
                 var formData = new FormData();
                 formData.append('_token', "{{ csrf_token() }}");
                 formData.append('old_file', '{{auth()->user()->resume}}');
@@ -62,14 +59,7 @@
                     contentType: false,
                     success: function(response) {
                         if (response.code == "200") {
-
-                            Swal.fire({
-                              title: 'Resume change successfully',
-                              text: '',
-                              icon: 'success',
-                              showCancelButton: false,
-                              confirmButtonText: 'OK'
-                            })
+                            toastr.success('Resume change successfully')
 
                             setTimeout(function() {
                                 window.location.href = '{{url('/applicant/resume')}}'
@@ -79,13 +69,10 @@
                         }
                     },
                     error: function(xhr, status, error) {
-                        // Handle the AJAX request error
                         var result = JSON.parse(xhr.responseText)
                         displayErrors(result.errors)
                     }
                 });
-        
             })
-
-        </script>
+    </script>
 @endsection
