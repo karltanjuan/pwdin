6a5367ad67995ff5d0d2ad7e2550ba8de2b65f44
Karl Angelo Tanjuan <tanjuankarlangelo2015@gmail.com>
2023-12-01 00:22:41 +0800

    Fixed bug

--- resources/views/employer/view-job.blade.php
+++ resources/views/employer/view-job.blade.php
@@ -8,9 +8,9 @@
     <li class="breadcrumb-item text-white active">Job Details</li>
 @endsection
 @section('content')
-    @if(!isset($job->employer)) 
-        @php 
-            redirect("{{url('/employer/jobs')}}")
+    @if(optional($job->employer)->company === null)
+        @php
+            return redirect(url('/employer/jobs'));
         @endphp
     @endif
     <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
