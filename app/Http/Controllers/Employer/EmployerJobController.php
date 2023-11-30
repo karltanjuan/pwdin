f62ac1074537ab99a418e372bf63fb62a1be5071
Karl Angelo Tanjuan <tanjuankarlangelo2015@gmail.com>
2023-11-30 23:46:08 +0800

    Added employer id checking for job list

--- app/Http/Controllers/Employer/EmployerJobController.php
+++ app/Http/Controllers/Employer/EmployerJobController.php
@@ -21,9 +21,12 @@ use Carbon\Carbon;
 class EmployerJobController extends Controller
 {
     public function index() {
-        $jobs = Job::orderBy('created_at', 'desc')
+        $id   = auth()->guard('employers')->user()->id;
+        $jobs = Job::where('employer_id', $id)
+                ->orderBy('created_at', 'desc')
                 ->with('applications')
                 ->get();
+                
         return view('employer.jobs', compact('jobs'));
     }
 
